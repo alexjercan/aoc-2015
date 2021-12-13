18ce4cd127e02b13b0d6b6b5ba1d07dba4cb0ce6
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-13 20:29:24 +0200

    added day 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,15 +1,39 @@
 module Day02 where
 
-type Input = String
+import Util (Parser, integerP, charP, parseList)
+import Control.Arrow ((&&&))
+
+data Rectangle = Rectangle Int Int Int deriving Show
+type Input = [Rectangle]
+
+rectangleParser :: Parser Rectangle
+rectangleParser = do
+    a <- integerP
+    charP 'x'
+    b <- integerP
+    charP 'x'
+    Rectangle a b <$> integerP
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = parseList rectangleParser . lines
 
-solve1 :: Input -> ()
-solve1 = const ()
+area :: Rectangle -> Int
+area (Rectangle a b c) = (*2) $ sum [a*b, a*c, b*c]
 
-solve2 :: Input -> ()
-solve2 = const ()
+slack :: Rectangle -> Int
+slack (Rectangle a b c) = minimum [a*b, a*c, b*c]
+
+volume :: Rectangle -> Int
+volume (Rectangle a b c) = product [a, b, c]
+
+ribbon :: Rectangle -> Int
+ribbon (Rectangle a b c) = (*2) $ minimum [a+b, a+c, b+c]
+
+solve1 :: Input -> Int
+solve1 = sum . map ((+) . area <*> slack)
+
+solve2 :: Input -> Int
+solve2 = sum . map ((+) . ribbon <*> volume)
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
--- src/Util.hs
+++ src/Util.hs
@@ -1,6 +1,23 @@
 module Util where
 
 import Data.Char (isSpace)
+import Text.Parsec (Parsec, many1, digit, letter, char)
+import Text.Parsec.Error (ParseError)
+import qualified Text.Parsec as Parsec
 
 rstrip :: [Char] -> [Char]
 rstrip = reverse . dropWhile isSpace . reverse
+
+type Parser = Parsec String ()
+
+parse :: Parser a -> String -> Either ParseError a
+parse p = Parsec.parse p ""
+
+parseList :: Parser a -> [String] -> [a]
+parseList p = either (error . show) id . mapM (parse p)
+
+integerP :: Parser Int
+integerP = read <$> many1 digit
+
+charP :: Char -> Parser Char
+charP = char
