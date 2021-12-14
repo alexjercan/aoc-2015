9250c2715fb38c29fb28851c77da0f16f8f17314
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-14 20:53:22 +0200

    added day 03

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,15 +1,67 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Day03 where
 
-type Input = String
+import Util (charP, Parser, manyP, parse, counter)
+import Control.Applicative ((<|>))
+import Control.Arrow ((&&&))
+import qualified Data.Map as M
+
+data Position a = Position a a deriving (Show, Ord, Eq, Functor)
+
+instance Applicative Position where
+    pure a = Position a a
+    (<*>) (Position fa fb) (Position a b) = Position (fa a) (fb b)
+
+instance Semigroup a => Semigroup (Position a) where
+    (<>) (Position a b) (Position c d) = Position (a <> c) (b <> d)
+
+instance Monoid a => Monoid (Position a) where
+    mempty = Position mempty mempty
+
+instance Semigroup Int where
+    (<>) = (+)
+
+instance Monoid Int where
+  mempty = 0
+
+data Direction = Up | LeftD | RightD | Down deriving Show
+
+toPosition :: Direction -> Position Int
+toPosition Up = Position 0 1
+toPosition LeftD = Position (-1) 0
+toPosition RightD = Position 1 0
+toPosition Down = Position 0 (-1)
+
+directionP :: Parser Direction
+directionP = Up <$ charP '^' <|> LeftD <$ charP '<' <|> RightD <$ charP '>' <|> Down <$ charP 'v'
+
+everyOther :: [a] -> ([a], [a])
+everyOther xs = go xs [] []
+    where go [] xs ys = (reverse xs, reverse ys)
+          go (x:y:zs) xs ys = go zs (x:xs) (y:ys)
+          go _ _ _ = undefined
+
+evenInd :: [a] -> [a]
+evenInd (x:_:xs) = x : evenInd xs
+evenInd _ = []
+
+oddInd :: [a] -> [a]
+oddInd (_:y:ys) = y : oddInd ys
+oddInd _ = []
+
+type Input = [Direction]
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = either (error . show) id . parse (manyP directionP)
 
-solve1 :: Input -> ()
-solve1 = const ()
+solution :: [Direction] -> [Position Int]
+solution = scanl1 (<>) . (Position 0 0:) . map toPosition
 
-solve2 :: Input -> ()
-solve2 = const ()
+solve1 :: [Direction] -> Int
+solve1 = length . counter . solution
+
+solve2 :: [Direction] -> Int
+solve2 = length . counter . ((++) <$> solution . evenInd <*> solution . oddInd)
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
--- src/Util.hs
+++ src/Util.hs
@@ -1,12 +1,26 @@
 module Util where
 
 import Data.Char (isSpace)
-import Text.Parsec (Parsec, many1, digit, letter, char)
+import Text.Parsec (Parsec, many1, digit, letter, char, upper, space, string)
 import Text.Parsec.Error (ParseError)
 import qualified Text.Parsec as Parsec
+import qualified Data.Map as M
+import Data.List (transpose)
 
 rstrip :: [Char] -> [Char]
 rstrip = reverse . dropWhile isSpace . reverse
+
+interleave :: [a] -> [a] -> [a]
+interleave xs ys = concat (transpose [xs, ys])
+
+counter :: Ord a => [a] -> M.Map a Int
+counter = foldl (\m x -> M.insertWith (+) x 1 m) M.empty
+
+counterW :: Ord a => [(a, Int)] -> M.Map a Int
+counterW = foldl (\m (x, v) -> M.insertWith (+) x v m) M.empty
+
+mkPairs :: [a] -> [(a, a)]
+mkPairs = zip <*> tail
 
 type Parser = Parsec String ()
 
@@ -19,5 +33,21 @@ parseList p = either (error . show) id . mapM (parse p)
 integerP :: Parser Int
 integerP = read <$> many1 digit
 
+upperSP :: Parser String
+upperSP = many1 upper
+
 charP :: Char -> Parser Char
 charP = char
+
+stringP :: String -> Parser String
+stringP = string
+
+upperP :: Parser Char
+upperP = upper
+
+spaceP :: Parser Char
+spaceP = space
+
+manyP :: Parser a -> Parser [a]
+manyP = many1
+
