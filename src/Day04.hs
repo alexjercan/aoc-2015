2cee107e79b6751e9f45dd05f7104d2d72890b5f
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-20 20:24:29 +0200

    added day 4

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,15 +1,26 @@
 module Day04 where
 
+import Data.ByteString.Conversion ( toByteString )
+import Control.Arrow ((&&&))
+import Util.Extra (rstrip)
+import Data.Digest.Pure.MD5 ( md5 )
+
 type Input = String
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = rstrip
 
-solve1 :: Input -> ()
-solve1 = const ()
+firstNZero :: Int -> [Char] -> Bool
+firstNZero n = all ('0' ==) . take n
 
-solve2 :: Input -> ()
-solve2 = const ()
+checkNumber :: Int -> String -> Int -> Bool
+checkNumber n s = firstNZero n . show . md5 . toByteString . (s ++) . show
+
+solve1 :: String -> Int
+solve1 s = head $ filter (checkNumber 5 s) [1..]
+
+solve2 :: String -> Int
+solve2 s = head $ filter (checkNumber 6 s) [1..]
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
