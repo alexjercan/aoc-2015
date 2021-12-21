44f728e729893e2921ad83c7b5e070b75a2cf96a
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-21 19:51:58 +0200

    added day 05

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,15 +1,49 @@
 module Day05 where
+import Control.Arrow (Arrow((&&&)))
+import Data.List (isInfixOf, group)
 
-type Input = String
+type Input = [String]
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = lines
 
-solve1 :: Input -> ()
-solve1 = const ()
+naughtySubstrings :: [String]
+naughtySubstrings = ["ab", "cd", "pq", "xy"]
 
-solve2 :: Input -> ()
-solve2 = const ()
+vowels :: String
+vowels = "aeiou"
+
+containsNaughtySubstring :: String -> Bool
+containsNaughtySubstring x = any (`isInfixOf` x) naughtySubstrings
+
+containsThreeVowels :: String -> Bool
+containsThreeVowels = (>=3) . length . filter (`elem` vowels)
+
+containsCharTwiceInARow :: String -> Bool
+containsCharTwiceInARow = any ((>1) . length) . group
+
+isNice :: String -> Bool
+isNice x = not (containsNaughtySubstring x) && containsThreeVowels x && containsCharTwiceInARow x
+
+solve1 :: [String] -> Int
+solve1 = length . filter isNice
+
+hasThreePalindrome :: String -> Bool
+hasThreePalindrome (a:rs'@(b:c:rs)) = a == c || hasThreePalindrome rs'
+hasThreePalindrome _ = False
+
+containsOrdered :: Char -> Char -> String -> Bool
+containsOrdered x y (a:rs'@(b:rs)) = (x == a && y == b) || containsOrdered x y rs'
+containsOrdered _ _ _ = False
+
+hasNonOverlapPairs :: String -> Bool
+hasNonOverlapPairs (x:rs'@(y:rs)) = containsOrdered x y rs || hasNonOverlapPairs rs'
+hasNonOverlapPairs _ = False
+
+isNice' :: String -> Bool
+isNice' x = hasThreePalindrome x && hasNonOverlapPairs x
+
+solve2 = length . filter isNice'
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
