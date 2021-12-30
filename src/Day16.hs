232460ee729c9dfd08c539a8c307f265c75b0d33
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-30 19:45:56 +0200

    added day 16

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,15 +1,65 @@
 module Day16 where
 
-type Input = String
+import Control.Arrow (Arrow((&&&)))
+import qualified Data.Map.Strict as M
+import Text.Parsec (sepBy, string)
+import Util.Parser (Parser, intP, parseList, word)
+
+clueP :: Parser (String, Int)
+clueP = (,) <$> word <*> (string ": " >> intP)
+
+auntP :: Parser (M.Map String Int)
+auntP = do
+    _ <- string "Sue " >> intP >> string ": "
+    M.fromList <$> clueP `sepBy` string ", "
+
+knownClues :: M.Map String Int
+knownClues =
+    M.fromList
+        [ ("children", 3)
+        , ("cats", 7)
+        , ("samoyeds", 2)
+        , ("pomeranians", 3)
+        , ("akitas", 0)
+        , ("vizslas", 0)
+        , ("goldfish", 5)
+        , ("trees", 3)
+        , ("cars", 2)
+        , ("perfumes", 1)
+        ]
+
+knownClues' :: M.Map String [Int]
+knownClues' =
+    M.fromList
+        [ ("children", [3])
+        , ("cats", [8 .. 100])
+        , ("samoyeds", [2])
+        , ("pomeranians", [0 .. 2])
+        , ("akitas", [0])
+        , ("vizslas", [0])
+        , ("goldfish", [0 .. 4])
+        , ("trees", [4 .. 100])
+        , ("cars", [2])
+        , ("perfumes", [1])
+        ]
+
+type Input = [M.Map String Int]
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = parseList auntP . lines
 
-solve1 :: Input -> ()
-solve1 = const ()
+checkOne :: M.Map String Int -> Bool
+checkOne = M.foldlWithKey (\acc k v -> acc && (knownClues M.! k == v)) True
 
-solve2 :: Input -> ()
-solve2 = const ()
+solve1 :: [M.Map String Int] -> Integer
+solve1 = head . map fst . filter (checkOne . snd) . zip [1 ..]
+
+checkOne' :: M.Map String Int -> Bool
+checkOne' =
+    M.foldlWithKey (\acc k v -> acc && (v `elem` knownClues' M.! k)) True
+
+solve2 :: [M.Map String Int] -> Integer
+solve2 = head . map fst . filter (checkOne' . snd) . zip [1 ..]
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
