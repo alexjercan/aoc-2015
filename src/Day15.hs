16c94ce5a10e93adf6636fc1a00169c8f13d63b3
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-30 19:23:14 +0200

    added day 15

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,15 +1,44 @@
 module Day15 where
 
-type Input = String
+import Control.Arrow (Arrow((&&&)))
+import Data.List (transpose)
+import Text.Parsec (string)
+import Util.Parser (Parser, intP, parseList, word)
+
+ingredientP :: Parser [Int]
+ingredientP = do
+    _ <- word >> string ": capacity "
+    c <- intP
+    _ <- string ", durability "
+    d <- intP
+    _ <- string ", flavor "
+    f <- intP
+    _ <- string ", texture "
+    t <- intP
+    _ <- string ", calories "
+    c' <- intP
+    return [c, d, f, t, c']
+
+type Input = [[Int]]
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = parseList ingredientP . lines
 
-solve1 :: Input -> ()
-solve1 = const ()
+solution :: (Ord b, Num b, Enum b) => [[b]] -> [(b, b)]
+solution xs =
+    [ (product . take 4 &&& last) $
+    map (max 0 . sum . zipWith (*) [i, j, k, l]) (transpose xs)
+    | i <- [0 .. 100]
+    , j <- [0 .. 100 - i]
+    , k <- [0 .. 100 - i - j]
+    , l <- [100 - i - j - k]
+    ]
 
-solve2 :: Input -> ()
-solve2 = const ()
+solve1 :: [[Int]] -> Int
+solve1 = maximum . map fst . solution
+
+solve2 :: [[Int]] -> Int
+solve2 = maximum . map fst . filter ((== 500) . snd) . solution
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
