7ea61838d1722efe3e7775358cd3ef58b80787e2
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-12-30 19:55:43 +0200

    added day 17

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,15 +1,24 @@
 module Day17 where
 
-type Input = String
+import Control.Arrow (Arrow((&&&)))
+import Data.List (subsequences, minimumBy)
+import Data.Function (on)
+
+type Input = [Int]
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = map read . lines
 
-solve1 :: Input -> ()
-solve1 = const ()
+solution :: [Int] -> [[Int]]
+solution = filter ((==150) . sum) . subsequences
 
-solve2 :: Input -> ()
-solve2 = const ()
+solve1 :: [Int] -> Int
+solve1 = length . solution
+
+solve2 :: [Int] -> Int
+solve2 xs = length $ filter ((==length mx) . length) xs'
+    where xs' = solution xs
+          mx = minimumBy (compare `on` length) xs'
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
