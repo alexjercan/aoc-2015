284e595415e6429e9005fc44a13472307861cdad
Alexandru Jercan <jercan_alex27@yahoo.com>
2022-01-02 16:06:46 +0200

    added day 18

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,15 +1,49 @@
 module Day18 where
 
-type Input = String
+import Control.Arrow (Arrow((&&&)))
+import qualified Data.Set as S
 
-parseContent :: String -> Input
-parseContent = id
+countNeighbors :: S.Set (Int, Int) -> (Int, Int) -> Int
+countNeighbors s (x, y) =
+    length $
+    filter (`S.member` s) $
+    [(i, j) | i <- [x - 1 .. x + 1], j <- [y - 1 .. y + 1], i /= x || j /= y]
 
-solve1 :: Input -> ()
-solve1 = const ()
+type Input = [[Char]]
 
-solve2 :: Input -> ()
-solve2 = const ()
+parseContent = lines
+
+rule :: S.Set (Int, Int) -> (Int, Int) -> Bool
+rule s p
+    | p `S.member` s = n == 2 || n == 3
+    | otherwise = n == 3
+  where
+    n = countNeighbors s p
+
+step :: Int -> Int -> S.Set (Int, Int) -> S.Set (Int, Int)
+step n m s = foldl (\acc p -> if rule s p then S.insert p acc else acc) S.empty indices
+  where
+    indices = [(i, j) | i <- [0 .. n - 1], j <- [0 .. m - 1]]
+
+onCorners :: Int -> Int -> S.Set (Int, Int) -> S.Set (Int, Int)
+onCorners n m s = foldr S.insert s cs
+    where cs = [(0, 0), (0, m-1), (n-1, 0), (n-1, m-1)]
+
+solve1 ms = length $ (!!100) $ iterate (step n m) s
+  where
+    n = length ms
+    m = length $ head ms
+    s =
+        S.fromList
+            [(i, j) | i <- [0 .. n - 1], j <- [0 .. m - 1], ms !! i !! j == '#']
+
+solve2 ms = length $ (!!100) $ iterate (onCorners n m . step n m . onCorners n m) s
+  where
+    n = length ms
+    m = length $ head ms
+    s =
+        S.fromList
+            [(i, j) | i <- [0 .. n - 1], j <- [0 .. m - 1], ms !! i !! j == '#']
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
