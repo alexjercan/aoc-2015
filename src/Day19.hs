268dd8faa0610f727e8ed777538449280963f499
Alexandru Jercan <jercan_alex27@yahoo.com>
2022-01-04 20:04:52 +0200

    added day 19

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,15 +1,43 @@
 module Day19 where
 
-type Input = String
+import Control.Arrow (Arrow((&&&)))
+import Data.Bifunctor (Bifunctor(bimap, second))
+import Data.List.Split (splitOn)
+import qualified Data.Map.Strict as M
+import Text.Parsec (string)
+import Util.Input (first2)
+import Util.Parser (Parser, parseList, word)
+import Data.List(group, nub, intersperse)
+import Data.Char (isUpper)
+
+replacementP :: Parser (String, String)
+replacementP = (,) <$> word <*> (string " => " >> word)
+
+type Input = (M.Map String [String], String)
 
 parseContent :: String -> Input
-parseContent = id
+parseContent =
+    bimap
+        (M.fromListWith (++) . map (second (: [])) . parseList replacementP)
+        head .
+    first2 . splitOn [""] . lines
 
-solve1 :: Input -> ()
-solve1 = const ()
+insertAt :: String -> String -> [String] -> Int -> String
+insertAt p s xs n = concat $ intersperse p (take n xs) ++ [s] ++ intersperse p (drop n xs)
 
-solve2 :: Input -> ()
-solve2 = const ()
+replacements :: String -> String -> String -> [String]
+replacements p s text = map (insertAt p s xs) [1 .. n - 1]
+    where xs = splitOn p text
+          n  = length xs
+
+solve1 (ms, s) = length $ nub $ M.foldlWithKey (\acc k as -> acc ++ concatMap (\a -> replacements k a s) as) [] ms
+
+solve2 :: Input -> Int
+solve2 (_, s) = n - rn - ar - 2 * y - 1
+          where rn = length (splitOn "Rn" s) - 1
+                ar = length (splitOn "Ar" s) - 1
+                y = length (splitOn "Y" s) - 1
+                n = length $ filter isUpper s
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
