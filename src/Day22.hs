6809d516433036e57b04b2d62883c7a9b3c808e5
Alexandru Jercan <jercan_alex27@yahoo.com>
2022-01-08 22:30:56 +0200

    added day 23
    day 22 is still a fail, saw a solution of 500 lines of code sadge

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,15 +1,31 @@
 module Day22 where
 
-type Input = String
+import Control.Arrow (Arrow((&&&)))
+import Text.Parsec (space, string)
+import Util.Parser (Parser, intP, parse)
+
+data Effect =
+    Effect Int (Stats -> Stats)
+
+data Stats = Stats Int Int Int Int deriving Show
+
+data Player =
+    Player Stats [Effect]
+
+playerP :: Parser Stats
+playerP = do
+    hp <- string "Hit Points: " >> (intP <* space)
+    dmg <- string "Damage: " >> (intP <* space)
+    return $ Stats hp dmg 0 0
+
+type Input = Stats
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = either (error . show) id . parse playerP
 
-solve1 :: Input -> ()
-solve1 = const ()
+solve1 = id
 
-solve2 :: Input -> ()
 solve2 = const ()
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
--- src/Day23.hs
+++ src/Day23.hs
@@ -1,15 +1,63 @@
+{-# LANGUAGE TupleSections #-}
 module Day23 where
 
-type Input = String
+import Control.Applicative (Alternative((<|>)))
+import Text.Parsec.Char (char, letter, string)
+import Util.Parser (Parser, intP, parseList)
+import Control.Arrow (Arrow((&&&)))
+import Control.Monad.State (State, modify, get, evalState)
+import qualified Data.Map.Strict as M
+
+data Instr
+    = Hlf Char
+    | Tpl Char
+    | Inc Char
+    | Jmp Int
+    | Jie Char Int
+    | Jio Char Int
+    deriving (Show)
+
+instrP :: Parser Instr
+instrP =
+    Hlf <$> (string "hlf " >> letter) <|> Tpl <$> (string "tpl " >> letter) <|>
+    Inc <$> (string "inc " >> letter) <|>
+    (char 'j' >>
+     (Jmp <$> (string "mp " >> intP) <|>
+      (char 'i' >>
+       (Jie <$> (string "e " >> letter) <*> (string ", " >> intP) <|>
+        (Jio <$> (string "o " >> letter) <*> (string ", " >> intP))))))
+
+type Input = [Instr]
 
 parseContent :: String -> Input
-parseContent = id
+parseContent = parseList instrP . lines
 
-solve1 :: Input -> ()
-solve1 = const ()
+type Comp = (M.Map Char Int, Int, [Instr])
+type Eval = State Comp
 
-solve2 :: Input -> ()
-solve2 = const ()
+eval :: M.Map Char Int -> Int -> Instr -> (M.Map Char Int, Int)
+eval reg ip (Hlf c) = (M.insertWith (flip div) c 2 reg, ip + 1)
+eval reg ip (Tpl c) = (M.insertWith (*) c 3 reg, ip + 1)
+eval reg ip (Inc c) = (M.insertWith (+) c 1 reg, ip + 1)
+eval reg ip (Jmp i) = (reg, ip + i)
+eval reg ip (Jie c i) = (reg, ip + if even (reg M.! c) then i else 1)
+eval reg ip (Jio c i) = (reg, ip + if 1 == (reg M.! c) then i else 1)
+
+step :: Comp -> Comp
+step (reg, ip, is) = (reg', ip', is)
+    where instr = is !! ip
+          (reg', ip') = eval reg ip instr
+
+solveM :: Char -> Eval Int
+solveM c = do
+    (reg, ip, is) <- get
+    if ip < 0 || ip >= length is
+       then return $ reg M.! c
+       else modify step >> solveM c
+
+solve1 = evalState (solveM 'b') . (M.fromList [('a', 0), ('b', 0)], 0,)
+
+solve2 = evalState (solveM 'b') . (M.fromList [('a', 1), ('b', 0)], 0,)
 
 solve :: String -> String
-solve = id
+solve = show . (solve1 &&& solve2) . parseContent
